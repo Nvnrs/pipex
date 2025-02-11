fd6465f6a366ade1d13ee0c1c6de285979fba38b
Nvnrs <nicolasvenerosy@gmail.com>
2025-02-11 18:11:31 +0100

    mandatory part is done

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: nveneros <nveneros@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/10 13:31:17 by nveneros          #+#    #+#             */
-/*   Updated: 2025/02/11 14:46:11 by nveneros         ###   ########.fr       */
+/*   Updated: 2025/02/11 18:11:11 by nveneros         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -127,25 +127,38 @@ char *get_path_cmd(char **split_cmd, char **envp)
 	return (path_cmd);
 }
 
+int	len_split(char **tab)
+{
+	int i;
+
+	i = 0;
+	while (tab[i])
+		i++;
+	return (i);
+}
+
 void	run_cmd(char *cmd, char **envp)
 {
 	char **split_cmd;
 	char *path_cmd;
-	char **args_cmd;
 
 	split_cmd =  ft_split(cmd, ' ');
+	if (len_split(split_cmd) == 0)
+	{
+		free(split_cmd);
+		exit(EXIT_FAILURE);
+	}
 	path_cmd = get_path_cmd(split_cmd, envp);
-	args_cmd = &split_cmd[1];
 	if (path_cmd == NULL)
 	{
 		ft_putstr_fd("Cmd not found :", 2);
 		ft_putstr_fd(split_cmd[0], 2);
+		ft_putstr_fd("\n", 2);
 		free_split(split_cmd);
 		exit(EXIT_FAILURE);
 	}
-	if (execve(path_cmd, args_cmd, envp) == -1)
+	if (execve(path_cmd, split_cmd, envp) == -1)
 	{
-		perror(NULL);
 		free(path_cmd);
 		free_split(split_cmd);
 		exit(EXIT_FAILURE);
@@ -158,40 +171,59 @@ void	first_process_child(char *path_infile, char *cmd, int *pipe_fd, char **envp
 	infile = open(path_infile, O_RDONLY);
 	if (infile < 0)
 	{
+		close(pipe_fd[0]);
+		close(pipe_fd[1]);
 		perror(path_infile);
 		exit(EXIT_FAILURE);
 	}
 	close(pipe_fd[0]);
 	dup2(infile, STDIN_FILENO);
-	// dup2(pipe_fd[1], STDOUT_FILENO);
+	dup2(pipe_fd[1], STDOUT_FILENO);
 	close(infile);
 	close(pipe_fd[1]);
 	run_cmd(cmd, envp);
 }
 
+void	last_process_child(char *path_outfile, char *cmd, int *pipe_fd, char **envp)
+{
+	int file;
 
+	file = open(path_outfile, O_CREAT| O_WRONLY | O_TRUNC);
+	if (file < 0)
+	{
+		close(pipe_fd[0]);
+		close(pipe_fd[1]);
+		perror(path_outfile);
+		exit(EXIT_FAILURE);
+	}
+	close(pipe_fd[1]);
+	dup2(pipe_fd[0], STDIN_FILENO);
+	dup2(file, STDOUT_FILENO);
+	close(file);
+	close(pipe_fd[0]);
+	run_cmd(cmd, envp);
+}
 
 int	main(int argc, char **argv, char **envp)
 {
 	int pid1;
+	int pid2;
 	int fd1[2];
 
 	if (argc != 5 || argv == NULL)
 		return (ft_putstr_fd("Format is : infile cmd1 cmd2 outfile\n", 2), EXIT_FAILURE);
-		
-	// first process_child
 	if (pipe(fd1) == -1)
 		return perror(NULL), (EXIT_FAILURE);
 	pid1 = fork();
 	if (pid1 == 0)
 		first_process_child(argv[1], argv[2], fd1, envp);
-	else
-	{
-		close(fd1[0]);
-		close(fd1[1]);
-		waitpid(pid1, NULL, 0);
-	}
-
+	pid2 = fork();
+	if (pid2 == 0)
+		last_process_child(argv[4], argv[3], fd1, envp);
+	close(fd1[1]);
+	close(fd1[0]);
+	waitpid(pid1, NULL, 0);
+	waitpid(pid2, NULL, 0);
 	return (EXIT_SUCCESS);
 }
 
